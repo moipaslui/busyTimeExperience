57cd71cb719cc93e7c463eb94abed8570b4c87f9
nom <v.meriau45@gmail.com>
2019-11-20 13:53:38 +0100

    adresse serveur1

--- syncMesureClient.py
+++ syncMesureClient.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST1 = "192.168.1.126"
+HOST1 = "192.168.1.207"
 HOST2 = ""
 HOST3 = ""
 HOST4 = ""
@@ -10,6 +10,6 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s :
     s.connect((HOST1, PORT))
-    s.connect((HOST2, PORT))
-    s.connect((HOST3, PORT))
-    s.connect((HOST4, PORT))
+    #s.connect((HOST2, PORT))
+    #s.connect((HOST3, PORT))
+    #s.connect((HOST4, PORT))
