877a4df6c147f3f61e29e5242e9b713bb21cfc25
lafdal <v.meriau45@gmail.com>
2019-11-21 14:21:36 +0100

    serveur mosar2 et 3

--- syncMesureServeurMosar2.py
+++ syncMesureServeurMosar2.py
@@ -5,7 +5,7 @@ import socket
 import subprocess as sp
 import time
 
-HOST = '192.168.1.207'  # Standard loopback interface address (localhost)
+HOST = '192.168.1.120'
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 INTERFACE = "wlan0"
 TEMPSMESURE = 10
@@ -17,17 +17,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.listen()
 		conn, addr = s.accept()
 		with conn:
-			print('Connected by', addr)
-			# while True:
 			sp.run(["nmcli nm wifi off"], shell = True) #setup du Mosar en mode monitor
 			sp.run(["sudo ifconfig " + INTERFACE + " down"], shell = True)
 			sp.run(["sudo iwconfig " + INTERFACE + " mode monitor"], shell = True)
 			sp.run(["sudo rfkill unblock wlan"], shell = True)
 			sp.run(["sudo ifconfig " + INTERFACE + " up"], shell = True)
 			sp.run(["sudo iwconfig " + INTERFACE + " channel 1"], shell = True)
-			time.sleep(10) #il faut sleep suffisamment le temps que l'iperf se déclenche mais pas trop pour ne pas aller à la fin de l'iperf
+			time.sleep(5) #il faut sleep suffisamment le temps que l'iperf se déclenche mais pas trop pour ne pas aller à la fin de l'iperf
 			calculeBusyTime("busyTimeTotal.txt", INTERFACE, TEMPSMESURE)
-			time.sleep(10) #on sleep suffisamment longtemps pour attendre la fin de l'iperf
+			time.sleep(20) #on sleep suffisamment longtemps pour attendre la fin de l'iperf
 			calculeBusyTime("busyTimeAmbiant.txt", INTERFACE, TEMPSMESURE)
 			sp.run(["nmcli nm wifi on"], shell = True) #ne pas oublier de configurer network-manager pour que OpenWrt soit automatiquement sélectionné
 
