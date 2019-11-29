63c35498900e4c52148a56a7490bf98e229c95f6
lafdal <moi.pas.lui45@gmail.com>
2019-11-29 13:45:21 +0100

    serveurs qui fonctionennt

--- serveurMosar8.py
+++ serveurMosar8.py
@@ -7,9 +7,11 @@ import time
 import datetime
 
 HOST = "192.168.1.78"
-PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 INTERFACE = "wlan0"
 TEMPSMESURE = 20
+nombreMesure = 5
+i = 0
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST, PORT))
@@ -23,12 +25,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 			sp.run(["sudo rfkill unblock wlan"], shell = True)			
 			sp.run(["sudo ifconfig " + INTERFACE + " up"], shell = True)
 			sp.run(["sudo iwconfig " + INTERFACE + " channel 1"], shell = True)
-			time.sleep(5) #il faut sleep suffisamment le temps que l'iperf se déclenche mais pas trop pour ne pas aller à la fin de l'iperf
-			busyTimeTotal = calculeBusyTime(INTERFACE, TEMPSMESURE)
-			time.sleep(20) #on sleep suffisamment longtemps pour attendre la fin de l'iperf
-			busyTimeAmbiant = calculeBusyTime(INTERFACE, TEMPSMESURE)
-			debitIPerf = recv(2048)
-			fichierRecap = open(str(datetime.day) + "/" + str(datetime.month) + "/" + str(datetime.year) + "-Mosar8-" + debitIPerf, "a")
-			fichierRecap.write(busyTimeTotal + "\n")
-			fichierRecap.write(busyTimeAmbiant + "\n")
+			while i < nombreMesure :
+				if(time.localtime().tm_min % 3 == 0) : #on sychronise le client et le serveur
+					time.sleep(5) #il faut sleep suffisamment le temps que l'iperf se déclenche mais pas trop pour ne pas aller à la fin de l'iperf
+					print("debutBusyTime")					
+					busyTimeTotal = calculeBusyTime(INTERFACE, TEMPSMESURE)
+					time.sleep(20) #on sleep suffisamment longtemps pour attendre la fin de l'iperf
+					print("debutAmbiant")
+					busyTimeAmbiant = calculeBusyTime(INTERFACE, TEMPSMESURE)
+					fichierRecap = open(str(datetime.date.today().day) + "-" + str(datetime.date.today().month) + "-" + str(datetime.date.today().year) + "_Mosar8.txt", "a")
+					fichierRecap.write(str(busyTimeTotal) + " " + str(busyTimeAmbiant) + "\n")
+					i+=1
+			fichierRecap.close()
+			i = 0
 
